8e4f57c62f419bc269a53cab33b045693830fe3b
rmEleven <1569323975@qq.com>
2023-08-28 16:58:13 +0800

    实现 urls->views

--- FireBall/urls.py
+++ FireBall/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('game.urls')),  # 默认使用 game 的 urls
     path('admin/', admin.site.urls),
 ]
