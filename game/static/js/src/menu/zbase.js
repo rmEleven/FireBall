dadaa504ee4d9f50b35d270db8b9912447b76ab6
rmEleven <1569323975@qq.com>
2023-08-30 11:11:44 +0800

    初始化菜单界面

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -10,13 +10,4 @@ class GameMenu {
         `);
         this.root.$game.append(this.$menu);  // 把menu前端添加到game前端
     }
-}class Game {
-    constructor(id) {
-        console.log("Game Created");  // 游戏创建的提示信息
-
-        this.id = id;              // 传入的前端div的id
-        this.$game = $('#' + id);  // 找到id对应的前端div
-
-        this.menu = new GameMenu(this);  // 创建menu对象
-    }
 }
