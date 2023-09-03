a5119d9423bbc00650faf5e67ae947ed5620f35c
rmEleven <1569323975@qq.com>
2023-09-03 21:50:26 +0800

    补充上次commit

--- static/js/src/zbase.js
+++ static/js/src/zbase.js
@@ -1,10 +1,12 @@
 export class Game {
-    constructor(id) {
+    constructor(id, AcwingOS) {
         console.log("Game Created");  // 游戏创建的提示信息
 
         this.id = id;              // 传入的前端div的id
         this.$game = $('#' + id);  // 找到id对应的前端div
+        this.AcwingOS = AcwingOS;  // 在Acwing中调用
 
+        this.settings = new Settings(this);         // 创建settings对象
         this.menu = new GameMenu(this);             // 创建menu对象
         this.playground = new GamePlayground(this)  // 创建playground对象
 
