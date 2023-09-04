21cf6b20c9baeef5f3c73255c146c3482299d358
rmEleven <1569323975@qq.com>
2023-09-04 11:42:24 +0800

    设计登录界面的前端样式

--- static/js/src/settings/zbase.js
+++ static/js/src/settings/zbase.js
@@ -10,6 +10,49 @@ class Settings {
         this.username = '';  // 用户名
         this.photo = '';  // 头像
 
+        // 创建settings前端
+        this.$settings = $(`
+            <div class="game-settings">
+
+                <div class="game-settings-login">
+                    <br>
+
+                    <div class="game-settings-field">
+                        <div class="game-settings-item">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                                <path
+                                d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+                            </svg>
+                            <input type="text" class="game-settings-input" placeholder="学工号" id="username" />
+                        </div>
+                    </div>
+
+                    <div class="game-settings-field">
+                        <div class="game-settings-item">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
+                                <path
+                                d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z" />
+                            </svg>
+                            <input type="password" class="game-settings-input" placeholder="密码" id="password" />
+                        </div>
+                    </div>
+
+                    <button type="submit" class="btn btn-success">登录</button>
+
+                    <p class="text">注册账号</p>
+                </div>
+
+                <div class="game-settings-register">
+                </div>
+
+            </div>
+        `);
+        this.$login = this.$settings.find(".game-settings-login");        // 找出login部分
+        this.$login.hide();
+        this.$register = this.$settings.find(".game-settings-register");  // 找出register部分
+        this.$register.hide();
+        this.root.$game.append(this.$settings);                           // 把settings前端添加到game前端
+
         this.start();
     }
 
@@ -18,11 +61,13 @@ class Settings {
     }
 
     login() {  // 打开登录界面
-        ;
+        this.$register.hide();  // 隐藏注册部分
+        this.$login.show();     // 显示登录部分
     }
 
     register() {  // 打开注册界面
-        ;
+        this.$login.hide();     // 隐藏登录部分
+        this.$register.show();  // 显示注册部分
     }
 
     getinfo() {  // 从服务器端获取信息
@@ -51,11 +96,11 @@ class Settings {
 
     show() {
         // 显示settings页面
-        //this.$settings.show();
+        this.$settings.show();
     }
 
     hide() {
         // 隐藏settings页面
-        //this.$settings.hide();
+        this.$settings.hide();
     }
 }
