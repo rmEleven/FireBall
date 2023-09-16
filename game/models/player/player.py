2c3b7156f791739bc86f402bfe1c61f745a39ab5
rmEleven <1569323975@qq.com>
2023-09-16 20:02:38 +0800

    玩家类添加openid信息

--- game/models/player/player.py
+++ game/models/player/player.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)  # 删除User会删除对应的Player
     photo = models.URLField(max_length=512, blank=True)
+    openid = models.CharField(default="", max_length=50, blank=True, null=True)
 
     def __str__(self):
         return str(self.user)
